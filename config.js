ddf1ec63b2c0da92b4526aa7856bd6c785d9aedc
Thiendeptrai386 <93469195+Thiendeptrai386@users.noreply.github.com>
2021-11-03 15:51:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const nameGirl = 'Thiện đẹp trai';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng noenl';
-const titleCard = 'Thiện';
+const eventName = 'Chúc Mừng SN';
+const titleCard = 'Thiện đz';
 const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
