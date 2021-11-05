c57d39f3dd36f78fa6ed42bb040960585fbe61ad
Thiendeptrai386 <93469195+Thiendeptrai386@users.noreply.github.com>
2021-11-05 13:29:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const nameGirl = 'Dương Thúy Huỳnh';
+const nameGirl = 'thien dz';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Sinh Nhật';
+const eventName = 'thiện đep trai';
 const titleCard = 'Dương Thúy Huỳnh';
 const contentCard = 'Chúc honey của anh 16/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
