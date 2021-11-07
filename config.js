571a5ccd72d261a23e1b81cd486ac654d12a8bdb
Thiendeptrai386 <93469195+Thiendeptrai386@users.noreply.github.com>
2021-11-07 16:14:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const nameGirl = 'thien đz';
+const nameGirl = 'Gửi vợ tương lai';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'thiện đep sn';
-const titleCard = 'thiện đẹp trai';
-const contentCard = 'Chúc honey của anh 16/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const eventName = 'Merry Chistmas';
+const titleCard = 'Noel';
+const contentCard = 'Chúc honey của anh 25/12 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
