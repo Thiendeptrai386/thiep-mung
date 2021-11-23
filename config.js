d4951e6d0e38aa373099378acb50904e29b2cdb2
Thiendeptrai386 <93469195+Thiendeptrai386@users.noreply.github.com>
2021-11-23 09:29:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const giftUrl = 'http://nodemy.vn';
 const eventName = 'Merry Chistmass';
 const titleCard = 'Master Thien';
 const contentCard = 'xã hội lầy chỉ có làm chịu khó cần cù thì bù xiêng năng chỉ có làm thì mới có ăn những cái loại không làm mà đòi có ăn thì ăn đb ăn cứt
-n ǝ^o1 !';
+n ǝ^o1. !';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
